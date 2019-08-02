c067f28aad5e51f125b1f10871fb63913d8c8712
Aaaaash <binshao54@gmail.com>
2019-08-02 15:48:23 +0800

    update options

--- src/options.ts
+++ src/options.ts
@@ -1,9 +1,31 @@
-import { getExtensionStorage } from './storage';
+import { getExtensionStorage, updateExtensionStorage } from './storage';
 
 async function startup(): Promise<void> {
     const storage = await getExtensionStorage();
-    Object.keys(storage).forEach((configKey: string) => {
-        console.log(`${configKey}: ${storage[configKey]}.`);
+    const disposes = Object.keys(storage).map((configKey: string) => {
+        const element: HTMLInputElement = document.querySelector(`.${configKey}`);        
+        if (element) {
+            console.log(`Key: ${configKey}, Value: ${storage[configKey]}`);
+            element.checked = storage[configKey];
+            const changeHandler = async (e: MouseEvent): Promise<void> => {
+                console.log(`Update -> ${(e.target as HTMLInputElement).checked}`);
+                await updateExtensionStorage({
+                    ...storage,
+                    [configKey]: (e.target as HTMLInputElement).checked,
+                });
+            }
+            element.addEventListener('change', changeHandler);
+            return () => {
+                element.removeEventListener('change', changeHandler);
+            };
+        }
+        return () => {};
+    });
+
+    window.addEventListener('close', () => {
+        for (const dispose of disposes) {
+            dispose();
+        }
     });
 }
 
